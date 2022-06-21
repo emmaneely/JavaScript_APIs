7cd5bef7d251592774728482de8234923b8a1f99
emmaneely <emmaneely3@gmail.com>
2022-06-21 18:10:15 -0500

    Completed exercise

--- app.js
+++ app.js
@@ -2,3 +2,31 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const APP_ID = "oLHVguiBplCRFoXcBwHnHA9QUD8IcFJp";
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+
+let searchBtn = document.getElementById("searchBtn");
+let searchField = document.getElementById("searchField");
+
+searchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let img = document.querySelector("img");
+    let userSearch = searchField.value;
+
+    fetch(`${GIPHY_URL}?&s=${userSearch}&api_key=${APP_ID}`, { mode: "cors" })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            console.log(result);
+            img.src = result.data.images.original.url;
+            img.alt = result.data.title;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    searchField.value = "";
+});
